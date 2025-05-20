cc348168034bb47f26f7d1716e136b8dc1b6d448
Lasitha Harshana <lasitha6646@gmail.com>
2025-05-21 01:57:24 +0530

    add camera layout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,4 +56,7 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    //CameraX dependencies
+    implementation(libs.androidx.camera.camera2)
 }
--- app/src/main/java/com/example/caloriecam/MainActivity.kt
+++ app/src/main/java/com/example/caloriecam/MainActivity.kt
@@ -4,27 +4,62 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.caloriecam.ui.theme.CalorieCamTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val cameraPermissionRequest =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            if (isGranted) {
+                // Implement camera related  code
+            } else {
+                // Camera permission denied
+            }
+
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             CalorieCamTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "HEllow",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                        Column(
+                            modifier = Modifier
+                                .padding(innerPadding)
+                                .fillMaxSize()
+                        ) {
+                            Box(
+                                modifier = Modifier
+                                    .weight(1f)
+                                    .fillMaxWidth()
+                            ) {
+                                Text("Camera Preview Here", modifier = Modifier.align(alignment = Alignment.Center))
+                            }
+                            Button(
+                                onClick = { /* TODO: Implement camera action */ },
+                                modifier = Modifier
+                                    .padding(16.dp)
+                                    .fillMaxWidth()
+                            ) {
+                                Text("Open Camera")
+                            }
+                        }
+                    }
             }
         }
     }
